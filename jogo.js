77d9b4e59d1fcd614955c766ba20cbf631682d83
marcosx3 <marcos-oliveirasilva@hotmail.com>
2021-04-09 20:37:42 -0300

    0.0.2

--- jogo.js
+++ jogo.js
@@ -6,35 +6,41 @@ function ajustaTela() {
 	altura = window.innerHeight
 	largura = window.innerWidth
 
-	
+	console.log(altura, largura)
 }
 
 ajustaTela()
 
 
 function posicaoRandomica() {
-	var posicaoX = Math.floor(Math.random() * largura) - 90
-	var posicaoY = Math.floor(Math.random() * altura) - 90
-// testando se for menor que 0
+	//se ja existir mosquito e excluido
+	if (document.getElementById('mosquito')) {
+		document.getElementById('mosquito').remove()
+	}
+
+	var posicaoX = Math.floor(Math.random() * largura) - 120
+	var posicaoY = Math.floor(Math.random() * altura) - 120
+	console.log(posicaoX, posicaoY)
+	// testando se for menor que 0
 	posicaoX = posicaoX < 0 ? 0 : posicaoX
 	posicaoY = posicaoY < 0 ? 0 : posicaoY
 
-								//criar o elemento html
+	//criar o elemento html
 	var mosquito = document.createElement('img')
 	mosquito.src = 'imagens/mosquito.png'
-	mosquito.className = tamanhoAleatorio() + ' ' + ' ' + ladoAleatorio()
+	mosquito.className = tamanhoAleatorio() + ' ' + ladoAleatorio()
 	mosquito.style.left = posicaoX + 'px'
 	mosquito.style.top = posicaoY + 'px'
-	
+	mosquito.id = 'mosquito'
 	mosquito.style.position = 'absolute'
 	document.body.appendChild(mosquito)
-	
+
 }
 
-function tamanhoAleatorio(){
+function tamanhoAleatorio() {
 	var classe = Math.floor(Math.random() * 3)
 
-	switch(classe){
+	switch (classe) {
 		case 0:
 			return 'mosquito'
 		case 1:
@@ -43,15 +49,15 @@ function tamanhoAleatorio(){
 			return 'mosquito2'
 	}
 }
-function ladoAleatorio(){
+function ladoAleatorio() {
 	var lado = Math.floor(Math.random() * 2)
 
-	switch(lado){
+	switch (lado) {
 		case 0:
 			return 'ladoA'
 		case 1:
 			return 'ladoB'
-		
+
 	}
 
 }
