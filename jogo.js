9bedc686ff8631836e1a6a4edb67c90ad60b056a
marcosx3 <marcos-oliveirasilva@hotmail.com>
2021-04-09 21:31:55 -0300

    0.0.3

--- jogo.js
+++ jogo.js
@@ -1,7 +1,9 @@
 
 var altura = 0
 var largura = 0
-
+var vidas = 1
+var tempo = 10
+var score = 0
 function ajustaTela() {
 	altura = window.innerHeight
 	largura = window.innerWidth
@@ -10,12 +12,32 @@ function ajustaTela() {
 }
 
 ajustaTela()
-
+var cronometro = setInterval(function () {
+	document.getElementById('tempo').innerHTML  = tempo
+	tempo -= 1
+	if(tempo <0){
+		clearInterval(cronometro)
+		clearInterval(mosca)
+		window.location.href = 'victory.html'
+	}else {
+		document.getElementById('tempo').innerHTML  = tempo
+		score += 1
+		document.getElementById('score').innerText  = score
+	}
+}, 2000)
 
 function posicaoRandomica() {
 	//se ja existir mosquito e excluido
 	if (document.getElementById('mosquito')) {
 		document.getElementById('mosquito').remove()
+		if(vidas > 3) {
+
+			window.location.href = 'end_game.html'
+		} else {
+			document.getElementById('life' + vidas).src = "imagens/coracao_vazio.png"
+
+			vidas++
+		}
 	}
 
 	var posicaoX = Math.floor(Math.random() * largura) - 120
@@ -33,8 +55,11 @@ function posicaoRandomica() {
 	mosquito.style.top = posicaoY + 'px'
 	mosquito.id = 'mosquito'
 	mosquito.style.position = 'absolute'
+	
+	mosquito.onclick = function(){
+		this.remove()
+	}
 	document.body.appendChild(mosquito)
-
 }
 
 function tamanhoAleatorio() {
